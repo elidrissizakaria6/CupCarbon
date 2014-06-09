7e9672b27b9f2ed0dd28f8e368c7b0abe8cd27e3
bounceur <bounceur@univ-brest.fr>
2014-06-09 21:13:05 +0200

    Update.

---
+++ CupCarbon/src/tracking/TargetWS.java
@@ -0,0 +1,68 @@
+package tracking;
+
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class TargetWS {
+	
+	private static BufferedReader br = null;
+	
+	public static void clean() {	
+		try {
+			URL url = new URL("http://pagesperso.univ-brest.fr/~bounceur/cupcarbonws/clean.php");			
+			url.openStream();
+		} catch (IOException e) {
+			e.printStackTrace();			
+		}
+	}
+	
+	public static void create(String id, double la, double lo) {	
+		try {
+			URL url = new URL("http://pagesperso.univ-brest.fr/~bounceur/cupcarbonws/add_target.php?id="+id+"&la="+la+"&lo="+lo);			
+			url.openStream();
+		} catch (IOException e) {
+			e.printStackTrace();			
+		}
+	}
+	
+	public static String getInfo(String id) {	
+		try {
+			URL url = new URL("http://pagesperso.univ-brest.fr/~bounceur/cupcarbonws/get_target.php?id="+id);			
+			br = new BufferedReader(new InputStreamReader(url.openStream()));
+			String s = br.readLine();
+			br.close();
+			return s;
+		} catch (IOException e) {
+			e.printStackTrace();			
+		}
+		return null;
+	}
+		
+	public static void displayCoords(String id) {
+		String str = getInfo(id);
+		String [] strT = str.split("\"");
+		System.out.println(strT[3]);
+		System.out.println(strT[7]);
+		System.out.println(strT[11]);
+	}
+	
+	public static Point getCoords(String id) {
+		Point p = new Point(0,0) ;
+		String str = getInfo(id);
+		String [] strT = str.split("\"");
+		p.setLocation(Double.valueOf(strT[7]), Double.valueOf(strT[11]));
+		return p;
+	}
+	
+	public static void setCoords(String id, double la, double lo) {
+		try {
+			URL url = new URL("http://pagesperso.univ-brest.fr/~bounceur/cupcarbonws/update_target.php?id="+id+"&la="+la+"&lo="+lo);			
+			url.openStream();
+		} catch (IOException e) {
+			e.printStackTrace();			
+		}	
+	}
+}
