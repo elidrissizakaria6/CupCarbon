dfee5e2ad6cc42e8eb44b2444c512bcaba8f1ed4
EL IDRISSI <elidrissizakaria6@gmail.com>
2015-05-04 16:29:05 +0200

    Améliorations de l'algorithme BIPAdapté

--- CupCarbon/src/perso/BIPAdapteA.java
+++ CupCarbon/src/perso/BIPAdapteA.java
@@ -13,28 +13,15 @@ import map.Layer;
 import device.DeviceList;
 import device.SensorNode;
 
-public class BIPAdapte extends Thread {
+public class BIPAdapteA extends Thread {
 
 	public void run() {
 		boolean marker=false;
 		double valeurMin= Double.MAX_VALUE;
-//		ArrayList<arete> aretes = new ArrayList<arete>();
 		List<SensorNode> capteurs = DeviceList.getSensorNodes();
 		List<SensorNode> noeudsNonMarques = new ArrayList<SensorNode>();
 		List<SensorNode> noeudsMarques = new ArrayList<SensorNode>();
 		System.out.println("je fais un truc");
-//		for (i=0;i<capteurs.size();i++) {
-//			SensorNode capteur=capteurs.get(i);
-//			
-//			for (int j=1;j<capteurs.size();j++) {
-//				SensorNode voisin=capteurs.get(j);
-//						aretes.add(new arete(capteur, voisin, capteur.distance(voisin)));		
-//			}
-//						
-//		}
-		
-//		Collections.sort(aretes);
-		//initialisation des noeuds
 		for( SensorNode a : capteurs )
 		{
 			a.setRadioRadius(0);
@@ -44,32 +31,41 @@ public class BIPAdapte extends Thread {
 		}
 		if(marker==false)
 		{
-			capteurs.get((1 + (int)(Math.random() * ((capteurs.size() - 2) + 1)))).setMarked(true);
+			SensorNode SN=capteurs.get((1 + (int)(Math.random() * ((capteurs.size() - 2) + 1))));
+			SN.setMarked(true);
+			noeudsMarques.add(SN);
 		}
 		SensorNode NoeudNonMarque=new SensorNode();
 		SensorNode NoeudMarque=new SensorNode();
 		SensorNode NoeudMarqueChoisi=new SensorNode();
-		while(noeudsMarques.size()<capteurs.size()){	
-			for(int i=0;i<capteurs.size();i++){
+		int i=0,j=0;
+		while(noeudsMarques.size()<capteurs.size()){
+//			for(int i=0;i<capteurs.size();i++){
+			i=0;
+			while(i<noeudsMarques.size()){
 				System.out.println("apres le permier for");
-				if(capteurs.get(i).isMarked()==true){
+				if(noeudsMarques.get(i).isMarked()==true){
 					System.out.println("mama");
-					NoeudMarque=capteurs.get(i);
-					for(int j=0;j<capteurs.size();j++){
+					NoeudMarque=noeudsMarques.get(i);
+//					for(int j=0;j<capteurs.size();j++){
+					j=0;
+					while(j<noeudsNonMarques.size()){
 						
-						if(capteurs.get(j).isMarked()==false){
+						if(noeudsNonMarques.get(j).isMarked()==false){
 							System.out.println("papa");
-							System.out.println("hana "+(NoeudMarque.Consommation(capteurs.get(j))-NoeudMarque.getValue()));
-							if(((NoeudMarque.Consommation(capteurs.get(j))-NoeudMarque.getValue())+(capteurs.get(j).Consommation(NoeudMarque)-capteurs.get(j).getValue()))<valeurMin)
+							System.out.println("hana "+(NoeudMarque.Consommation(noeudsNonMarques.get(j))-NoeudMarque.getValue()));
+							if(((NoeudMarque.Consommation(noeudsNonMarques.get(j))-NoeudMarque.getValue())+(noeudsNonMarques.get(j).Consommation(NoeudMarque)-noeudsNonMarques.get(j).getValue()))<valeurMin)
 								{	
-								System.out.println(NoeudMarque +" veut marqué " + capteurs.get(j));
-								NoeudNonMarque=capteurs.get(j);
+								System.out.println(NoeudMarque +" veut marqué " + noeudsNonMarques.get(j));
+								NoeudNonMarque=noeudsNonMarques.get(j);
 								NoeudMarqueChoisi=NoeudMarque;
-								valeurMin=(NoeudMarque.Consommation(NoeudNonMarque)-NoeudMarque.getValue())+(capteurs.get(j).Consommation(NoeudMarque)-capteurs.get(j).getValue());
+								valeurMin=(NoeudMarque.Consommation(NoeudNonMarque)-NoeudMarque.getValue())+(noeudsNonMarques.get(j).Consommation(NoeudMarque)-noeudsNonMarques.get(j).getValue());
 								}
 						}
+						j++;
 					}
 				}
+				i++;
 			}
 			System.out.println(NoeudMarqueChoisi +" a marqué " + NoeudNonMarque);
 			//Si on met pas le max, on va voir un noeud marqué diminuer sa valeur par rapport à un nouveau noeud non marqué 
